12f8709369cc226bc842725f4642287f533a130a
artlenk.ru@gmail.com <artlenk.ru@gmail.com>
2016-08-21 14:23:04 +0400

    Rewritten the whole structure markup

---
+++ old/gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Dev"
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
+
+
---
+++ src/js/scripts.js
@@ -0,0 +1,30 @@
+!function () {
+    $(window).load(function () {
+        $(".scroll-container").mCustomScrollbar({theme: "dark-thick", setHeight: "100%"})
+    })
+}(jQuery), $("#fold-filter").click(function () {
+    $(".wrapper-left").toggleClass("filter-hide open")
+}), $("#fold-list").click(function () {
+    $(".wrapper-left").toggleClass("establishments-hide open")
+}), $(".user-menu .sub-menu").click(function () {
+    $(".user-menu .sub-menu").toggleClass("open")
+}), $(document).ready(function () {
+    $("select").selectize({create: !0, sortField: "text"}), $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+    var e = $(".wrapper"), t = 1, i = $(".wrapper-left");
+    i.css("width", 50 / t + "%");
+    var n;
+    i.resizable({
+        handles: "e", start: function (e, t) {
+            n = t.originalSize.width + t.originalElement.next().outerWidth()
+        }, stop: function (t, i) {
+            var n = 100 * i.originalElement.outerWidth() / e.innerWidth();
+            i.originalElement.css("width", n + "%");
+            var l = i.originalElement.next(), o = 100 * l.outerWidth() / e.innerWidth();
+            l.css("width", o + "%")
+        }, resize: function (e, t) {
+            t.originalElement.next().width(n - t.size.width)
+        }
+    })
+});
